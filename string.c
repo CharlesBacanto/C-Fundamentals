25ac60d17c7b3fca25489feeb29149c95ecffd84
charlesdominique <charlesdominiquebacanto@gmail.com>
2025-03-05 15:37:24 +0800

    string

---
+++ string.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+
+    char string1[] = "Charles";
+    char string2[] = "SoftwareDeveloper";
+
+
+
+    // strlwr(string1); // lower case
+    // strupr(string1); // upper case
+    // strcat(string1,string2); // append
+    // strncat(string1,string2, 1); // append number
+    // strcpy(string1,string2);//copy
+    // strcpy(string1,string2. 2); // copy number
+
+    // strset(string1, '?'); // set all characters to given char
+    // strnset(string1, '?',2); // set number characters to given char
+    // strrev(string1)// reverse
+    //strcmp(string1,string2) // compare
+    //strncmp(string1,string2) // compare set number
+    //strcmpi(string1,string2) // compare ignore case
+    //strnicmp(string1,string2) // compare ignore case
+}   
